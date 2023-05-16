784c03a4ce416d3bb6fa5f92d991e62d404a68c5
Boobins <elias.albrektson@live.se>
2023-05-16 22:53:48 +0200

    Fixade cool effekt när man tänder facklan i mitten

--- TwinSuns/Assets/DevScenes/EliasScener/EliasSwitch/Scripts/ActivateTemple.cs
+++ TwinSuns/Assets/DevScenes/EliasScener/EliasSwitch/Scripts/ActivateTemple.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
 
 public class ActivateTemple : MonoBehaviour, IDamageable
 {
@@ -14,11 +16,27 @@ public class ActivateTemple : MonoBehaviour, IDamageable
     [SerializeField]
     private GameObject fire;
 
+    public UnityEvent fireLit;
+
+    [SerializeField]
+    private TMP_Text displayText;
+    [SerializeField]
+    private float timeBeforeText = 1f;
+    [SerializeField]
+    private float timeToRemoveText = 2f;
+    [SerializeField]
+    private float timeToOpenDoors = 2f;
+
 
     public void TakeDamage(int damage, DamageType dType = DamageType.Normal) //I detta fall ska den g� s�nder direkt �nd�
     {
         TorchHit(dType);
     }
+
+    private void Start()
+{
+    displayText.enabled = false;
+}
 
     private void TorchHit(DamageType damageType)
     {
@@ -27,15 +45,34 @@ public class ActivateTemple : MonoBehaviour, IDamageable
             case DamageType.Normal:
                 break;
             case DamageType.Fire:
-                door1.SetBool("IsUp", true);
-                door2.SetBool("IsUp", true);
+                Invoke("OpenDoors", timeToOpenDoors);
+                Invoke("DisplayTextRoutine", timeBeforeText);
                 fire.SetActive(true);
+                fireLit?.Invoke();
                 //här får ni lägga in något coolt med cameran och ljuset!
 
 
                 break;
         }
 
+    }
+
+    private void DisplayTextRoutine()
+{
+    displayText.enabled = true;
+    Invoke("RemoveText", timeToRemoveText);
+
+}
+
+  private void RemoveText()
+  {
+    displayText.enabled = false;
+  }
+
+    private void OpenDoors()
+    {
+      door1.SetBool("IsUp", true);
+      door2.SetBool("IsUp", true);
     }
 
 
