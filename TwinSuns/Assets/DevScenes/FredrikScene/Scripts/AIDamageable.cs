733ef5d6f3cbd3583c8e2d7569864594f233b8c7
Fredrik Rahmberg <fredrik.rahmberg@hotmail.se>
2023-05-22 15:14:35 +0200

    AI, fireball, player

--- TwinSuns/Assets/DevScenes/FredrikScene/Scripts/AIDamageable.cs
+++ TwinSuns/Assets/DevScenes/FredrikScene/Scripts/AIDamageable.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class AIDamageable : MonoBehaviour, IDamageable
 {
-    public int maxHealth = 100;
+    [SerializeField] private Shader whiteShader;  // The shader to switch to when hit
+    private Shader originalShader;  // The original shader of the enemy
+    private Renderer enemyRenderer;  // Reference to the enemy's renderer component
+
+    [SerializeField] private int maxHealth = 100;
     public int currentHealth;
+
+    [SerializeField] private float durationOfHit = 1f;
 
     private void Start()
     {
+        // Get the original shader from the renderer
+        enemyRenderer = GetComponent<Renderer>();
+        originalShader = enemyRenderer.material.shader;
+
         currentHealth = maxHealth;
     }
 
     public void TakeDamage(int damageAmount, DamageType dtype = DamageType.Normal)
     {
-        // You can add additional logic here to handle different damage types
+        StartCoroutine(ChangeShaderForDuration(whiteShader, durationOfHit));  // Switch to white shader for 1 second
         currentHealth -= damageAmount;
 
         if (currentHealth <= 0)
@@ -23,9 +33,20 @@ public class AIDamageable : MonoBehaviour, IDamageable
         }
     }
 
+    private IEnumerator ChangeShaderForDuration(Shader newShader, float duration)
+    {
+        // Switch to the new shader
+        enemyRenderer.material.shader = newShader;
+
+        // Wait for the specified duration
+        yield return new WaitForSeconds(duration);
+
+        // Revert back to the original shader
+        enemyRenderer.material.shader = originalShader;
+    }
+
     private void Die()
     {
-        // Implement logic for AI death
         Destroy(gameObject);
     }
 }
